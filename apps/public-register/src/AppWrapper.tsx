5b327438c8f831ff09fd951e95bd01a17f7f431d
Didier Hartong <info@focuswider.com>
2024-12-13 20:00:15 +0100

    fix: only build on public-register changes and ui. Remove console.log

--- apps/public-register/src/AppWrapper.tsx
+++ apps/public-register/src/AppWrapper.tsx
@@ -3,7 +3,6 @@ import Certificates from "./Certificates";
 export default function AppWrapper() {
   const rootElement = document.getElementById("root");
   const label = rootElement?.dataset.theme;
-  console.log("#DH# label", label);
   if (!rootElement) {
     return <div>Root element not found</div>;
   }
