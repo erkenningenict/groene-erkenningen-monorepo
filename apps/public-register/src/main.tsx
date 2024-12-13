2735520fbddf16cf6fc31b4e55adfe3b742c7e7c
Didier Hartong <info@focuswider.com>
2024-12-13 18:57:49 +0100

    fix: loading and overwriting the root element

--- apps/public-register/src/main.tsx
+++ apps/public-register/src/main.tsx
@@ -1,30 +1,9 @@
-// import { StrictMode } from "react";G
-
-// import { createRoot } from "react-dom/client";
-// // import "./tailwind.css";
-// import styles from "./tailwind.css?inline";
-// import AppWrapper from "./AppWrapper.tsx";
-
-// console.log("#DH# ", styles);
-// const host = document.createElement("my-app");
-// const shadow = host.attachShadow({ mode: "open" });
-
-// const sheet = new CSSStyleSheet();
-// sheet.replaceSync(styles);
-// shadow.adoptedStyleSheets = [sheet];
-
-// document.body.appendChild(host);
-// createRoot(document.getElementById("root")!).render(
-//   <StrictMode>
-//     <AppWrapper />
-//   </StrictMode>,
-// );
 import ReactDOM from "react-dom/client";
 import AppWrapper from "./AppWrapper";
 import styles from "./tailwind.css?inline";
 
 // Create a custom element
-class MyApp extends HTMLElement {
+class PublicRegister extends HTMLElement {
   constructor() {
     super();
     this.attachShadow({ mode: "open" });
@@ -40,8 +19,7 @@ class MyApp extends HTMLElement {
 
     // Create a div for React to render into
     const appRoot = document.createElement("div");
-    appRoot.id = "root";
-    // const appRoot = document.getElementById("root")!;
+
     this.shadowRoot!.appendChild(appRoot);
 
     // Render React app
@@ -51,12 +29,14 @@ class MyApp extends HTMLElement {
 }
 
 // Define the custom element
-customElements.define("my-app", MyApp);
+const appName = "fw-public-register";
+customElements.define(appName, PublicRegister);
 
 const theme = document.getElementById("root")!.dataset.theme;
 
-const ceApp = document.createElement("my-app");
-ceApp.dataset.theme = theme;
+const fwPublicRegister = document.createElement(appName);
+fwPublicRegister.dataset.theme = theme;
+fwPublicRegister.id = "root";
 
-// Add the custom element to the body
-document.getElementById("root")!.appendChild(ceApp);
+// Add the custom element to the root node
+document.getElementById("root")!.replaceWith(fwPublicRegister);
