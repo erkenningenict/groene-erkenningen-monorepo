eb5165be8f2ed2cfa8fced7daa5371ad62c3a69e
Didier Hartong <info@focuswider.com>
2024-12-20 15:43:37 +0100

    fix: make custom element

--- apps/calendar/src/main.tsx
+++ apps/calendar/src/main.tsx
@@ -1,7 +1,63 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// import "./tailwind.css";
+// import AppWrapper from "./AppWrapper.tsx";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <AppWrapper />
+//   </StrictMode>,
+// );
+
+import ReactDOM from "react-dom/client";
+import AppWrapper from "./AppWrapper";
+import styles from "./tailwind.css?inline";
+import { StrictMode } from "react";
+
+// Create a custom element
+class Calendar extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    // Create a style element
+    const style = document.createElement("style");
+    style.textContent = styles; // Assume 'styles' is imported or defined
+
+    // Append style to shadow DOM
+    this.shadowRoot!.appendChild(style);
+
+    // Create a div for React to render into
+    const appRoot = document.createElement("div");
+
+    this.shadowRoot!.appendChild(appRoot);
+
+    // Render React app
+    const root = ReactDOM.createRoot(appRoot);
+    root.render(
+      <StrictMode>
+        <AppWrapper />
+      </StrictMode>,
+    );
+  }
+}
+
+// Define the custom element
+const appName = "fw-calendar";
+customElements.define(appName, Calendar);
+
+const theme = document.getElementById("root")!.dataset.theme;
+
+const fwPublicRegister = document.createElement(appName);
+fwPublicRegister.dataset.theme = theme;
+fwPublicRegister.id = "root";
+
+// Add the custom element to the root node
+document.getElementById("root")!.replaceWith(fwPublicRegister);
+
 import "./tailwind.css";
-import AppWrapper from "./AppWrapper.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
